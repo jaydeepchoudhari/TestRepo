32aa4591562e0beabb4ef6213a796a311f783d09
jaydeepchoudhari <jpchoudhari@gmail.com>
2025-07-10 23:58:09 +0530

    Update obo.java

--- obo.java
+++ obo.java
@@ -1,108 +1,30 @@
-package duke.aigis.fho.uvt.migration.model;
+CREATE SCHEMA IF NOT EXISTS "SolarHealth";
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+CREATE TABLE "SolarHealth"."SOLAR_FARMS" (
+    "site_id" SERIAL PRIMARY KEY,
+    "site_name" VARCHAR(255),
+    "address" VARCHAR(255),
+    "active" BOOLEAN,
+    "Nickname" VARCHAR(255),
+    "current_state" INTEGER,
+    "geom" TEXT,
+    "panel_phototropic" BOOLEAN,
+    "Maximo_Site_Id" VARCHAR(255)
+);
 
-import javax.persistence.*;
+CREATE TABLE "SolarHealth"."SOLAR_FARMS_PANELS" (
+    "id" SERIAL PRIMARY KEY,
+    "geom" TEXT,
+    "site_id" SMALLINT NOT NULL,
+    "panel_id" VARCHAR(255),
+    "inverter" VARCHAR(255),
+    "combiner" VARCHAR(255),
+    "string" VARCHAR(255),
+    CONSTRAINT "fk_solar_farm_panel" 
+        FOREIGN KEY ("site_id") 
+        REFERENCES "SolarHealth"."SOLAR_FARMS" ("site_id")
+        ON DELETE CASCADE
+);
 
-@Data
-@Entity
-@Table(name = "SOLAR_FARMS_PANELS", schema = "SolarHealth")
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
-public class SolarFarmPanel {
-    
-    @Id
-    @Column(name = "id")
-    private int id;
-
-    @Column(name = "geom")
-    private String geom;
-
-    @Column(name = "site_id")
-    @JsonProperty("siteId")
-    private Short siteId;
-
-    @Column(name = "panel_id")
-    @JsonProperty("panelId")
-    private String panelId;
-
-    @Column(name = "inverter")
-    private String inverter;
-
-    @Column(name = "combiner")
-    private String combiner;
-
-    @Column(name = "string")
-    @JsonProperty("panelString")
-    private String panelString;
-
-}
-
-
-package duke.aigis.fho.uvt.migration.model;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-import org.hibernate.annotations.DynamicUpdate;
-
-import javax.persistence.*;
-import java.util.List;
-
-@Data
-@Entity
-@Table(name = "SOLAR_FARMS", schema = "SolarHealth")
-@ToString
-@NoArgsConstructor
-@AllArgsConstructor
-@DynamicUpdate
-public class SolarFarm {
-    
-    @Id
-    @Column(name = "site_id")
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @JsonProperty("siteId")
-    private Short siteId;
-
-    @Column(name = "site_name")
-    @JsonProperty("siteName")
-    private String siteName;
-
-    @Column(name = "address")
-    private String address;
-
-    @Column(name = "active")
-    @JsonProperty("isActive")
-    private  Boolean isActive;
-
-    @Column(name = "Nickname")
-    @JsonProperty("nickName")
-    private String nickName;
-
-    @Column(name = "current_state")
-    @JsonProperty("currentState")
-    private Integer currentState;
-    
-    @Column(name = "geom")
-    private String geom;
-
-    @Column(name = "panel_phototropic")
-    @JsonProperty("panelPhotographic")
-    private Boolean panelPhotographic;
-
-    @Column(name = "Maximo_Site_Id")
-    @JsonProperty("maximoSiteId")
-    private String maximoSiteId;
-    /*
-    @OneToMany(mappedBy = "siteId", fetch = FetchType.LAZY)
-    @JsonProperty("panels")
-    private List<SolarFarmPanel> panels; */
-    
-}
+CREATE INDEX "idx_solar_farms_panels_site_id" 
+    ON "SolarHealth"."SOLAR_FARMS_PANELS" ("site_id");
