59db23c011dc5fadf4fd357f2eb1a673d8aeab90
jaydeepchoudhari <jpchoudhari@gmail.com>
2025-07-10 14:06:16 -0400

    Update obo.java

--- obo.java
+++ obo.java
@@ -1,55 +1,108 @@
-public String callDevApiOnBehalfOfUser(String userAccessToken) {
-    try {
-        // Load from config or env vars
-        String clientId = "<prod-api-client-id>";
-        String clientSecret = "<prod-api-client-secret>";
-        String tenantId = "<tenant-id>";
-        String scope = "api://<dev-api-client-id>/.default"; // or user_impersonation
+package duke.aigis.fho.uvt.migration.model;
 
-        ConfidentialClientApplication app = ConfidentialClientApplication.builder(
-                clientId,
-                ClientCredentialFactory.createFromSecret(clientSecret)
-        )
-        .authority("https://login.microsoftonline.com/" + tenantId)
-        .build();
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-        UserAssertion userAssertion = new UserAssertion(userAccessToken);
+import javax.persistence.*;
 
-        OnBehalfOfParameters parameters = OnBehalfOfParameters.builder(
-                Collections.singleton(scope),
-                userAssertion
-        ).build();
+@Data
+@Entity
+@Table(name = "SOLAR_FARMS_PANELS", schema = "SolarHealth")
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class SolarFarmPanel {
+    
+    @Id
+    @Column(name = "id")
+    private int id;
 
-        IAuthenticationResult result = app.acquireToken(parameters).get();
+    @Column(name = "geom")
+    private String geom;
 
-        // Now call Dev/QA API with this token
-        String devToken = result.accessToken();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(devToken);
+    @Column(name = "site_id")
+    @JsonProperty("siteId")
+    private Short siteId;
 
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-        RestTemplate restTemplate = new RestTemplate();
+    @Column(name = "panel_id")
+    @JsonProperty("panelId")
+    private String panelId;
 
-        String devApiUrl = "https://dev-api.yoursite.com/endpoint";
-        ResponseEntity<String> response = restTemplate.exchange(devApiUrl, HttpMethod.GET, entity, String.class);
+    @Column(name = "inverter")
+    private String inverter;
 
-        return response.getBody();
+    @Column(name = "combiner")
+    private String combiner;
 
-    } catch (Exception e) {
-        e.printStackTrace();
-        return "Failed to call Dev API: " + e.getMessage();
-    }
+    @Column(name = "string")
+    @JsonProperty("panelString")
+    private String panelString;
+
 }
 
 
-com.microsoft.aad.msal4j.MsalInteractionRequiredException: AADSTS65001: The user or administrator has not consented to use the application with ID '2a318756-f3e6-4e01-8839-353d958262fa' named 'MOVES-Dev'. Send an interactive authorization request for this user and resource. Trace ID: b7fe26d3-a152-4456-9b2c-666f5db80400 Correlation ID: bfc1eaa3-5aea-4e55-b17c-c00b394ca15a Timestamp: 2025-06-19 18:47:14Z
+package duke.aigis.fho.uvt.migration.model;
 
-    https://login.microsoftonline.com/<tenant-id>/oauth2/v2.0/authorize?
-client_id=<MOVES-Dev-client-id>
-&response_type=code
-&redirect_uri=http://localhost
-&response_mode=query
-&scope=https://company.onmicrosoft.com/moves-qa-auth/.default
-&prompt=consent
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.DynamicUpdate;
 
-    http://localhost/?error=invalid_request&error_description=AADSTS9002325%3a+Proof+Key+for+Code+Exchange+is+required+for+cross-origin+authorization+code+redemption.+Trace+ID%3a+88419cf9-e04c-4a77-8424-b342ccd01100+Correlation+ID%3a+4d465c6a-b5b2-4ced-8910-9c55b772eda5+Timestamp%3a+2025-06-23+18%3a59%3a16Z
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "SOLAR_FARMS", schema = "SolarHealth")
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@DynamicUpdate
+public class SolarFarm {
+    
+    @Id
+    @Column(name = "site_id")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @JsonProperty("siteId")
+    private Short siteId;
+
+    @Column(name = "site_name")
+    @JsonProperty("siteName")
+    private String siteName;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "active")
+    @JsonProperty("isActive")
+    private  Boolean isActive;
+
+    @Column(name = "Nickname")
+    @JsonProperty("nickName")
+    private String nickName;
+
+    @Column(name = "current_state")
+    @JsonProperty("currentState")
+    private Integer currentState;
+    
+    @Column(name = "geom")
+    private String geom;
+
+    @Column(name = "panel_phototropic")
+    @JsonProperty("panelPhotographic")
+    private Boolean panelPhotographic;
+
+    @Column(name = "Maximo_Site_Id")
+    @JsonProperty("maximoSiteId")
+    private String maximoSiteId;
+    /*
+    @OneToMany(mappedBy = "siteId", fetch = FetchType.LAZY)
+    @JsonProperty("panels")
+    private List<SolarFarmPanel> panels; */
+    
+}
