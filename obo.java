5021bdf96dd1e8d78fba33a8c692196ca2fbd184
jaydeepchoudhari <jpchoudhari@gmail.com>
2025-08-04 12:34:05 -0400

    Update obo.java

--- obo.java
+++ obo.java
@@ -1,38 +1,89 @@
-create schema if not exists SolarHealth;
+//Existing Opex Locations
+package com.dukeenergy.formula.model.entities;
 
-create table SolarHealth.solar_farms (
-    site_id serial primary key,
-    site_name varchar(255),
-    address varchar(255),
-    active boolean,
-    nickname varchar(255),
-    current_state integer,
-    geom text,
-    panel_phototropic boolean,
-    maximo_site_id varchar(255),
-    created_by VARCHAR(255) NOT NULL,
-    created_ts TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    modified_by VARCHAR(255),
-    modified_ts TIMESTAMP WITH TIME ZONE
-);
+import lombok.Data;
 
-create table SolarHealth.solar_farms_panels (
-    id serial primary key,
-    geom text,
-    site_id smallint not null,
-    panel_id varchar(255),
-    inverter varchar(255),
-    combiner varchar(255),
-    string varchar(255),
-    created_by VARCHAR(255) NOT NULL,
-    created_ts TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    modified_by VARCHAR(255),
-    modified_ts TIMESTAMP WITH TIME ZONE
-    constraint fk_solar_farm_panel 
-        foreign key (site_id) 
-        references SolarHealth.solar_farms (site_id)
-        on delete cascade
-);
+import java.util.Date;
+import java.util.Set;
 
-create index idx_solar_farms_panels_site_id 
-    on SolarHealth.solar_farms_panels (site_id);
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "LOCATIONS")
+public class LocationEntity {
+    @Id
+    String uuid;
+    @Column(name = "site_id")
+    String siteId;
+    @Column(name = "site_name")
+    String siteName;
+    @Column(name = "address_1")
+    String address1;
+    @Column(name = "address_2")
+    String address2;
+    @Column(name = "city")
+    String city;
+    @Column(name = "state_code")
+    String stateCode;
+    @Column(name = "postal_code")
+    String postalCode;
+    @Column(name = "county")
+    String county;
+    @Column(name = "created")
+    Date created;
+    @Column(name = "created_by")
+    String createdBy;
+    @Column(name = "updated")
+    Date updated;
+    @Column(name = "updated_by")
+    String updatedBy;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "locationId")
+    private Set<BuildingEntity> buildings;
+}
+
+//Existing buildings:
+
+package com.dukeenergy.formula.model.entities;
+
+import lombok.Data;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "BUILDINGS")
+public class BuildingEntity {
+    @Id
+    String uuid;
+    @Column(name = "name")
+    String name;
+    @Column(name = "building_id")
+    Integer buildingId;
+    @Column(name = "building_code")
+    String buildingCode;
+    @Column(name = "location_id")
+    String locationId;
+}
+
+//Source of buildings
+
+package com.dukeenergy.formula.model.dto;
+
+public interface BuildingViewDTO {
+    String getSiteId();
+    String getBuildingId();
+    String getBuildingCode();
+    String getBuildingName();
+}
