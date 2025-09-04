5e7c0a8733e6b04edbfd4630e1f86c6f963b55cc
jaydeepchoudhari <jpchoudhari@gmail.com>
2025-09-05 00:34:44 +0530

    Update obo.java

--- obo.java
+++ obo.java
@@ -1,6 +1,9 @@
 @GetMapping("/api/solar/{plant_id}/inspections/all")
-    @ApiOperation(value="Gets all inspections for a given solar plant for a specified status")
-    public List<SolarInspectionView> getAllInspectionsForSolarPlant(@ApiParam(value="The Solar Plant Plant Id", required=true) @PathVariable short plant_id)
-    {
-        return solarInspectionViewService.getAllInspectionsForSolarPlantV3(plant_id);
-    }
+@ApiOperation(value="Gets all inspections for a given solar plant for a specified status")
+public List<SolarInspectionView> getAllInspectionsForSolarPlant(
+    @ApiParam(value="The Solar Plant Plant Id", required=true) @PathVariable short plant_id,
+    @ApiParam(value="Sort column name", defaultValue="created_ts") @RequestParam(required = false, defaultValue = "created_ts") String sortColumn,
+    @ApiParam(value="Sort direction (ASC/DESC)", defaultValue="DESC") @RequestParam(required = false, defaultValue = "DESC") String sortDirection)
+{
+    return solarInspectionViewService.getAllInspectionsForSolarPlantV3(plant_id, sortColumn, sortDirection);
+}
