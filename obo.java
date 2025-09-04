f36ab6d091a578331745ad2fa7e2a752814e74bd
jaydeepchoudhari <jpchoudhari@gmail.com>
2025-09-05 00:26:48 +0530

    Update obo.java

--- obo.java
+++ obo.java
@@ -1,2 +1,8 @@
- @Query(value = "select * from SOLAR.INSPECTION_VIEW where plant_id = ?1 order by created_ts desc", nativeQuery = true)
-    List<SolarInspectionView> findByPlantIdByOrderByCrtDateDesc(short plantId);
+@Query(value = "select * from SOLAR.INSPECTION_VIEW where plant_id = :plantId", 
+       nativeQuery = true)
+List<SolarInspectionView> findByPlantIdWithSort(
+    @Param("plantId") short plantId, 
+    Sort sort);
+
+Sort sort = Sort.by(Sort.Direction.DESC, "created_ts");
+List<SolarInspectionView> results = repository.findByPlantIdWithSort(plantId, sort);
