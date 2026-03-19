9232ab2ce3251e1894cd87b2fdaf0facc98e4353
jaydeepchoudhari <jpchoudhari@gmail.com>
2026-03-19 03:09:13 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
 curl --header "X-Vault-Token: hvs.CAESICapXQpltcGstqXjf5yU44TkWJG3n6AKYPHDgKgPbc5wGh4KHGh2cy5WU1JRbFduRnViTUZZVXA2bDkyOG9mc2w" --request GET https://vault-cc-dt.duke-energy.com/concourse/data/formula/database > response.json
 
 jq -r ".data.data.database.url, .data.data.database.user" response.json
+
+
+jq: parse error: Invalid numeric literal at line 1, column 3
