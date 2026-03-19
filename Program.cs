311385e65a6614e210829184e0da49d032bf5215
jaydeepchoudhari <jpchoudhari@gmail.com>
2026-03-19 03:00:07 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,1 @@
-curl --header "X-Vault-Token: $VAULT_TOKEN" --request GET $VAULT_ADDR/v1/secret/data/my-secret-password
+curl --header "X-Vault-Token: hvs.CAESICapXQpltcGstqXjf5yU44TkWJG3n6AKYPHDgKgPbc5wGh4KHGh2cy5WU1JRbFduRnViTUZZVXA2bDkyOG9mc2w" --request GET https://vault-cc-dt.duke-energy.com/concourse/data/formula/database > response.json
