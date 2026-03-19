f78c1e65f31a02660e60603dcb5c94418c6b1f3e
jaydeepchoudhari <jpchoudhari@gmail.com>
2026-03-19 12:24:02 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,1 @@
-curl -s \
-  -H "X-Vault-Token: $VAULT_TOKEN" \
-  $VAULT_ADDR/v1/concourse/data/formula/<secret-name> \
-  | jq -r '.data.data.database.url, .data.data.database.user'
+curl --header "X-Vault-Token: $VAULT_TOKEN" --request GET $VAULT_ADDR/v1/secret/data/my-secret-password
