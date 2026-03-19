af31efab93c4f2376bd093a6d5526aebfe7ef4e0
jaydeepchoudhari <jpchoudhari@gmail.com>
2026-03-19 12:16:30 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-{
-  "pass": "",
-  "url": "",
-  "user": ""
-}
+curl -s \
+  -H "X-Vault-Token: $VAULT_TOKEN" \
+  $VAULT_ADDR/v1/concourse/data/formula/<secret-name> \
+  | jq -r '.data.data.database.url, .data.data.database.user'
