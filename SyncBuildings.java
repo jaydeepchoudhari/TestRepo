8fe81a32d001f7e45051aded0d664f4333f86b8b
jaydeepchoudhari <jpchoudhari@gmail.com>
2026-04-06 13:20:59 -0400

    Update SyncBuildings.java

--- SyncBuildings.java
+++ SyncBuildings.java
@@ -1,58 +1,85 @@
-<div id="newFaultDialogHeader" class="card-header">
-  <div class="left-section">
-    <span id="note">Click And Drag To Create New Fault</span>
-    <span id="fileName">(20240321_175709_PCNemec_img_1.JPG)</span>
-  </div>
+eks-inframod-params: &eks-inframod-params
+  PRODUCT_NAME: ((product-name))
+  APPLICATION_NAME: ((application-name))
+  APPLICATION_BUILD_ENV: ((application-build-env))
+  APPLICATION_DEPLOY_ENV: ((application-deploy-env))
+  MANIFEST: ((application-deploy-manifest))
+  BRANCH: ((application-build-branch))
+  CODE_SCAN_USERNAME: ((checkmarx-scan-username))
+  CODE_SCAN_PASSWORD: ((checkmarx-scan-password))
+  VAULT_ADDR: ((vault-url-cc))
+  VAULT_TOKEN: ((vault-token-cc-rw))
+  EKS_CREDENTIALS: ((eks-credentials))
 
-  <div class="right-section">
-    <button class="nav-button prev">
-      <i class="fas fa-chevron-left"></i>
-    </button>
-    <button class="nav-button next">
-      <i class="fas fa-chevron-right"></i>
-    </button>
-  </div>
-</div>
+resources:
+  - name: ((application-build-branch))
+    type: git
+    icon: git
+    source:
+      uri: ((git-uri))
+      branch: ((application-build-branch))
+      skip_ssl_verification: true
+      username: ((github-readonly-username))
+      password: ((github-readonly-password))
+  - name: repo-task-eks-inframod
+    type: git
+    icon: cloud-lock
+    check_every: 30m
+    source:
+      uri: ((repo-task-app-eks-inframod-uri))
+      branch: ((repo-task-app-eks-inframod-version))
+      skip_ssl_verification: true
+  - name: build-image
+    type: registry-image
+    check_every: 10m
+    icon: docker
+    source:
+      repository: ((docker-base-image-host))/((docker-base-image-path))
+      tag: ((docker-base-image-tag))
+  - name: build-artifacts
+    type: s3
+    icon: nas
+    source:
+      endpoint: ((s3-onprem-endpoint))
+      disable_ssl: true
+      skip_ssl_verification: true
+      bucket: ((s3-onprem-bucket))
+      regexp: ((product-name))/((application-name))/((application-build-env))/(.*).zip
+      access_key_id: ((s3-onprem-access-key))
+      secret_access_key: ((s3-onprem-access-secret))
 
+jobs:
+  - name: job-update-pipeline
+    public: false
+    plan:
+      - get: ((application-build-branch))
+        params: { depth: 1 }
+        trigger: true
+      - set_pipeline: self
+        file: ((application-build-branch))/ci/pipeline-prod.yml
+        vars: { env-config: ((env-config)) }
+        var_files:
+          - ((application-build-branch))/ci/config/parameters-common.yml
+          - ((application-build-branch))/ci/config/parameters-((env-config)).yml
 
-  #newFaultDialogHeader {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 60px;
-  padding: 0 5%;
-  background-color: #fff; /* optional */
-}
-
-.left-section {
-  display: flex;
-  flex-direction: column; /* stack spans vertically */
-  justify-content: center;
-}
-
-#note {
-  font-weight: 600;
-}
-
-#fileName {
-  margin-top: 2px;
-  font-size: 0.9em;
-  color: #666;
-}
-
-.right-section {
-  display: flex;
-  align-items: center;
-  gap: 10px;
-}
-
-.nav-button {
-  background: none;
-  border: none;
-  cursor: pointer;
-  font-size: 18px;
-}
-
-.nav-button:hover {
-  color: #007bff;
-}
+  - name: job-deploy
+    public: false
+    serial: true
+    plan:
+      - in_parallel:
+          fail_fast: true
+          steps:
+            - get: build-image
+            - get: build-artifacts
+              params: { unpack: true }
+            - get: repo-task-eks-inframod
+              params: { depth: 1 }
+      - task: task-deploy
+        timeout: 20m
+        image: build-image
+        config:
+          platform: linux
+          inputs: [{ name: build-artifacts }, { name: repo-task-eks-inframod }]
+          params:
+            <<: *eks-inframod-params
+          run: { path: repo-task-eks-inframod/run.sh, args: ["deploy"] }
