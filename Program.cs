f3ec8e25b41d8f7bc5fb58f98ceccca6392a3354
jaydeepchoudhari <jpchoudhari@gmail.com>
2026-04-23 09:58:24 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,31 +1,44 @@
-name: Apigee Proxy Sync
+name: Apigee Proxy Deploy
 
 on:
   workflow_dispatch:
+    inputs:
+      api_org:
+        description: "The organization in the Apigee Hybrid environment you're targeting, e.g. de-apigee-dt"
+        required: true
+        type: choice
+        options:
+          - de-apigee-dt
+          - de-apigee-qa
+          - de-apigee-prod
+      api_env:
+        description: "Name of the apigee hybrid env used for syncing. Ex: dev-2"
+        required: true
+        type: choice
+        options:
+          - dev-10
+          - qa-10
+          - prod-10
+      parent_env:
+        description: >-
+          Name of the general environment in apigee hybrid. This would be something like "dev" or "qa"
+          instead of "dev-2" or "qa-10". This is necessary for grabbing secrets from vault that were migrated from the concourse vault for the apigee credentials.
+        required: true
+        type: choice
+        options:
+          - dev
+          - qa
+          - prod
 
 permissions:
   contents: read
   id-token: write
 
 jobs:
-  apigee-sync-proxy:
-    runs-on: ${{ vars.RUNNERSET_DEFAULT }}
-    permissions:
-      contents: read
-      id-token: write
-    steps:
-      - name: Checkout Repository
-        uses: actions/checkout@v6
-      - name: Vault Fetch private key
-        uses: dukeenergy-corp/duke-actions/composite/vault-fetch@master
-        with:
-          secrets: |
-            formula/bitbucket-repo-private-key | GH_PRIVATE_KEY
-      - name: Sync proxy
-        uses: dukeenergy-corp/duke-actions/composite/apigee-sync@master
-        with:
-          api-type: sharedflow
-          api-name: formula-common-resources
-          api-env: dev-10
-          parent-env: dev
-          api-name-ver: 
+  deploy-proxy:
+    uses: dukeenergy-corp/duke-actions/.github/workflows/apigee-proxy-deploy.yaml@master
+    with:
+      api-org: ${{ github.event.inputs.api_org }}
+      api-env: ${{ github.event.inputs.api_env }}
+      api-name: formula-common-resources
+      parent-env: ${{ github.event.inputs.parent_env }}
